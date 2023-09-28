fdf288cd87026bb120970ae5230262e56298ab9b
pmashi <nocsillty@gmail.com>
2023-09-28 19:11:11 -0400

    upd

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-const button = document.querySelector("button");
+const button = document.getElementById("1");
 
-button.addEventListener("click", updateName); 
+button.addEventListener("click", updateName);
+
+
+
 
 function updateName() { 
     const name = prompt("Enter a new name"); 
     button.textContent = `Player 1: ${name}`;
+}
+
+const para = document.getElementById("level");
+const levelUpBtn = document.getElementById("2"); 
+
+levelUpBtn.addEventListener("click", levelUp);
+
+
+function initNewPlayer() { 
+    var level = 1; 
+}
+
+function display() {
+    para.innerHTML = `Level: ${level}`;
+}
+
+function levelUp() { 
+    level++1; 
+    display(); 
 }
