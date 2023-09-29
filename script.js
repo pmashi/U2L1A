0fec17c7f50057f2b039ec0293b87c8e9127790d
Pei <nocsillty@gmail.com>
2023-09-28 22:19:39 -0400

    added level button

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
+// access Player button
 const button = document.getElementById("1");
-
 button.addEventListener("click", updateName);
-
-
-
 
 function updateName() { 
     const name = prompt("Enter a new name"); 
     button.textContent = `Player 1: ${name}`;
 }
 
+//access <p> tag "Level" and and button "levelup" 
 const para = document.getElementById("level");
 const levelUpBtn = document.getElementById("2"); 
-
 levelUpBtn.addEventListener("click", levelUp);
 
-
+var level; 
 function initNewPlayer() { 
-    var level = 1; 
+    level = 1; 
 }
 
 function display() {
-    para.innerHTML = `Level: ${level}`;
+    para.innerHTML = `Level: ${level}` + (level == 100 ? " (MAX)" : "");
 }
 
 function levelUp() { 
-    level++; 
+    if(level < 100) {
+        level++
+    }
     display(); 
 }
